a4cdc8da8da6a275ebb0de867111c6ec047d2586
josh26z <joshuamvarughese5284@gmail.com>
2025-09-02 09:34:34 +0000

    initial commit of shcalc.sh

---
+++ shcalc.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+splash()
+{
+echo
+echo
+sleep 0.1
+echo "################################################################################"
+sleep 0.1
+echo "################################################################################"
+sleep 0.1
+echo "# #                                                                          # #"
+sleep 0.1
+echo "# #                                                                          # #"
+sleep 0.1
+figlet -f smslant "SHITTY CALCULATOR   v1.0"
+sleep 0.1
+echo "# #                                                                          # #"
+sleep 0.1
+echo "# #                                                            --by srsu10d  # #"
+sleep 0.1
+echo "# ############################################################################ #"
+sleep 0.1
+echo "################################################################################"
+
+}
+
+options()
+{
+echo
+echo
+sleep 2
+echo "   lemme be frank... this ones a really shitty calculator... \n   i feel really sorry for u cos u chose this on urself"
+sleep 5
+echo
+echo "   devtip: CTRL^C NOW..."
+sleep 10
+echo
+echo "   [  Really?? okiee then... enjoiii... ] "
+sleep 1
+echo
+echo "   CHOOSE WHAT U WANNA DO...(u must really be having some free time)"
+echo
+sleep 1
+echo "		+ add "
+sleep 0.2
+echo "		- subtract"
+sleep 0.2
+echo "		* multiply"
+sleep 0.2
+echo "		/ divide"
+echo
+echo
+}
+
+splash
+options
